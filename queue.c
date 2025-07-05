6fd8e8f4794cdd8e1e06b7fc358dd19dad839840
Chris Feilbach <pointerwars2025@gmail.com>
2025-07-05 04:12:08 +0000

    Initial push from week2 repository to level2 repository

---
+++ queue.c
@@ -0,0 +1,10 @@
+#include "queue.h"
+
+// Function pointers to (potentially) custom malloc() and
+// free() functions.
+//
+//static void * (*malloc_fptr)(size_t size) = NULL;
+//static void   (*free_fptr)(void* addr)    = NULL; 
+
+// Implement your queue functions here.
+//
